c20f529b0d81f713e134f9350c68c821b5181aaa
Cris <crispr.zde@gmail.com>
2024-09-04 19:37:56 +0200

    created environment

---
+++ homeboard/src/main/java/com/homeboard/homeboard/HomeboardApplication.java
@@ -0,0 +1,14 @@
+package com.homeboard.homeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeboardApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ homeboard/src/test/java/com/homeboard/homeboard/HomeboardApplicationTests.java
@@ -0,0 +1,14 @@
+package com.homeboard.homeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
